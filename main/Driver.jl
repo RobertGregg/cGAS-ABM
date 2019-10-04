12d96a4357c31eb0bec30f3cddc560fd0477692a
RobertGregg <robwgregg@gmail.com>
2019-10-04 11:13:10 -0400

    Create file to run virus sims in parallel

--- main/Driver.jl
+++ main/Driver.jl
@@ -13,12 +13,11 @@ include("ModelSetup.jl")
 include("Callbacks.jl")
 
 #What type of infection are we performing?
-infectionType = :Virus #ISD, Virus, None
+infectionType = :None #ISD, Virus, None
 
 if infectionType == :ISD
     sol = @time solve(prob,CVODE_BDF(linear_solver=:GMRES),saveat=0.1)
 
 elseif infectionType == :Virus
     sol = @time solve(prob,CVODE_BDF(linear_solver=:GMRES),callback=cb_infect,tstops=tstop)
-
 end
